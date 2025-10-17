92b6e8accbd6bda3aca9b9b0a0cb67068569cfab
tracer-ysyx <tracer@ysyx.org>
2025-10-17 21:15:54 +0800

    > compile NEMU ysyx_xxxxxxxx MoonGrt Linux moon-vm 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:15:53 up 1 day,  2:56,  1 user,  load average: 0.85, 0.63, 0.50

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -18,6 +18,7 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "sdb.h"
+#include <utils.h>
 
 static int is_batch_mode = false;
 
@@ -55,6 +56,7 @@ static int cmd_q(char *args) {
 
 static int cmd_r(char *args) {
   init_isa();
+  nemu_state.state = NEMU_STOP;
   return 0;
 }
 
