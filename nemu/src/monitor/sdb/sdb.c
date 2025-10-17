3983b29097dc8bc11fb966b575bd8740250c064b
tracer-ysyx <tracer@ysyx.org>
2025-10-17 21:25:43 +0800

    > compile NEMU ysyx_xxxxxxxx MoonGrt Linux moon-vm 5.15.0-139-generic #149~20.04.1-Ubuntu SMP Wed Apr 16 08:29:56 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:25:43 up 1 day,  3:06,  1 user,  load average: 0.34, 0.29, 0.37

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -107,7 +107,7 @@ static int cmd_p(char *args) {
   bool success = true;
   uint32_t result = expr(args, &success);
   if (success) {
-    printf("%s = %x\n", args, result);
+    printf("%s = 0x%x\n", args, result);
   } else {
     printf("Invalid expression: %s\n", args);
   }
