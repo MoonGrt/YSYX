4775eb7e0909b9934c132659a9d4c73934f088ae
tracer-ysyx <tracer@ysyx.org>
2026-02-18 18:03:14 +0800

    > generate verilog ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:03:14 up  4:06,  1 user,  load average: 2.50, 1.04, 0.80

--- npc/playground/src/MiniRV/minirv.scala
+++ npc/playground/src/MiniRV/minirv.scala
@@ -1,5 +1,3 @@
-package minirv
-
 import chisel3._
 import chisel3.util._
 
@@ -8,18 +6,14 @@ import chisel3.util._
 // ---------------------------
 class IF extends Module {
   val io = IO(new Bundle {
-    val pc_next = Input(UInt(32.W))   // 下一个 PC
-    val pc_out  = Output(UInt(32.W))  // 当前 PC 输出
-    val instr   = Output(UInt(32.W))  // 当前指令
+    val pc_next = Input(UInt(32.W))    // 下一个 PC
+    val instr   = Input(UInt(32.W))    // 外部提供指令
+    val pc_out  = Output(UInt(32.W))   // 当前 PC 输出
   })
 
   // PC 寄存器
   val pc = RegInit(0.U(32.W))
   pc := io.pc_next
-
-  // 简单指令存储器 1K x 32bit
-  val imem = Mem(1024, UInt(32.W))
-  io.instr := imem(pc >> 2)  // 通过 PC 访问指令
   io.pc_out := pc
 }
 
@@ -28,23 +22,21 @@ class IF extends Module {
 // ---------------------------
 class ID extends Module {
   val io = IO(new Bundle {
-    val instr     = Input(UInt(32.W))  // 当前指令
-    val pc        = Input(UInt(32.W))  // 当前 PC
-    val rs1_data  = Output(UInt(32.W)) // 源寄存器 1 数据
-    val rs2_data  = Output(UInt(32.W)) // 源寄存器 2 数据
-    val rd_addr   = Output(UInt(5.W))  // 目的寄存器地址
-    val imm       = Output(UInt(32.W)) // 立即数
-    val alu_op    = Output(UInt(4.W))  // ALU 操作
-    val mem_read  = Output(Bool())     // 内存读信号
-    val mem_write = Output(Bool())     // 内存写信号
-    val reg_write = Output(Bool())     // 写回寄存器信号
-    val jalr      = Output(Bool())     // 是否 jalr 指令
+    val instr     = Input(UInt(32.W))
+    val pc        = Input(UInt(32.W))
+    val rs1_data  = Output(UInt(32.W))
+    val rs2_data  = Output(UInt(32.W))
+    val rd_addr   = Output(UInt(5.W))
+    val imm       = Output(UInt(32.W))
+    val alu_op    = Output(UInt(4.W))
+    val mem_read  = Output(Bool())
+    val mem_write = Output(Bool())
+    val reg_write = Output(Bool())
+    val jalr      = Output(Bool())
   })
 
-  // 通用寄存器堆 32x32
   val regfile = RegInit(VecInit(Seq.fill(32)(0.U(32.W))))
 
-  // 指令字段解析
   val opcode = io.instr(6,0)
   val rd     = io.instr(11,7)
   val funct3 = io.instr(14,12)
@@ -52,47 +44,40 @@ class ID extends Module {
   val rs2    = io.instr(24,20)
   val funct7 = io.instr(31,25)
 
-  // 读取寄存器值
   io.rs1_data := regfile(rs1)
   io.rs2_data := regfile(rs2)
   io.rd_addr  := rd
 
-  // 立即数生成
-  val imm_i = io.instr(31,20)          // I 型立即数
-  val imm_u = io.instr(31,12) << 12    // U 型立即数
-  val imm_s = Cat(io.instr(31,25), io.instr(11,7))  // S 型立即数
-
-  // 根据 opcode 选择立即数
+  val imm_i = io.instr(31,20)
+  val imm_u = io.instr(31,12) << 12
+  val imm_s = Cat(io.instr(31,25), io.instr(11,7))
   io.imm := MuxLookup(opcode, 0.U, Seq(
-    "b0010011".U -> imm_i,  // addi
-    "b0110111".U -> imm_u,  // lui
-    "b0000011".U -> imm_i,  // lw, lbu
-    "b0100011".U -> imm_s,  // sw, sb
-    "b1100111".U -> imm_i   // jalr
+    "b0010011".U -> imm_i,
+    "b0110111".U -> imm_u,
+    "b0000011".U -> imm_i,
+    "b0100011".U -> imm_s,
+    "b1100111".U -> imm_i
   ))
 
-  // ALU 操作码：0=add, 1=addi/lw/sw/lbu/sb, 2=lui
   io.alu_op := MuxLookup(opcode, 0.U, Seq(
-    "b0110011".U -> 0.U, // add
-    "b0010011".U -> 1.U, // addi
-    "b0110111".U -> 2.U, // lui
-    "b0000011".U -> 1.U, // lw, lbu
-    "b0100011".U -> 1.U, // sw, sb
-    "b1100111".U -> 1.U  // jalr
+    "b0110011".U -> 0.U,
+    "b0010011".U -> 1.U,
+    "b0110111".U -> 2.U,
+    "b0000011".U -> 1.U,
+    "b0100011".U -> 1.U,
+    "b1100111".U -> 1.U
   ))
 
-  // 内存读写信号
   io.mem_read  := (opcode === "b0000011".U)
   io.mem_write := (opcode === "b0100011".U)
   io.reg_write := (opcode === "b0110011".U || opcode === "b0010011".U ||
                    opcode === "b0000011".U || opcode === "b0110111".U || 
                    opcode === "b1100111".U)
-
   io.jalr := (opcode === "b1100111".U)
 }
 
 // ---------------------------
-// EX 模块：Execute/ALU
+// EX 模块
 // ---------------------------
 class EX extends Module {
   val io = IO(new Bundle {
@@ -104,64 +89,84 @@ class EX extends Module {
     val mem_addr= Output(UInt(32.W))
     val jalr    = Input(Bool())
     val pc      = Input(UInt(32.W))
-    val pc_next = Output(UInt(32.W)) // jalr 更新 PC
+    val pc_next = Output(UInt(32.W))
   })
 
-  // 简单 ALU
   io.alu_out := MuxLookup(io.alu_op, 0.U, Seq(
-    0.U -> (io.rs1 + io.rs2), // add
-    1.U -> (io.rs1 + io.imm), // addi, lw, sw, lbu, sb, jalr
-    2.U -> io.imm             // lui
+    0.U -> (io.rs1 + io.rs2),
+    1.U -> (io.rs1 + io.imm),
+    2.U -> io.imm
   ))
 
-  // 内存地址计算
   io.mem_addr := io.rs1 + io.imm
-
-  // jalr 指令 PC 更新
   io.pc_next := Mux(io.jalr, (io.rs1 + io.imm) & ~1.U(32.W), io.pc + 4.U)
 }
 
 // ---------------------------
-// Mini-RV 顶层 CPU 模块
+// ROM 模块（只读指令存储器）
 // ---------------------------
-class minirv extends Module {
+class ROM(size: Int) extends Module {
   val io = IO(new Bundle {
-    val mem_rdata = Input(UInt(32.W))  // 从内存读到的数据
-    val mem_wdata = Output(UInt(32.W)) // 写内存数据
-    val mem_addr  = Output(UInt(32.W)) // 内存地址
-    val mem_we    = Output(Bool())     // 写使能
+    val addr  = Input(UInt(32.W))
+    val data  = Output(UInt(32.W))
   })
 
-  // IF / ID / EX 模块实例化
-  val ifStage = Module(new IF)
-  val idStage = Module(new ID)
-  val exStage = Module(new EX)
+  val mem = Mem(size, UInt(32.W))
+  io.data := mem(io.addr >> 2)
+}
+
+// ---------------------------
+// RAM 模块（数据存储器）
+// ---------------------------
+class RAM(size: Int) extends Module {
+  val io = IO(new Bundle {
+    val addr  = Input(UInt(32.W))
+    val wdata = Input(UInt(32.W))
+    val rdata = Output(UInt(32.W))
+    val we    = Input(Bool())
+  })
+
+  val mem = Mem(size, UInt(32.W))
+  io.rdata := mem(io.addr >> 2)
+  when(io.we) { mem(io.addr >> 2) := io.wdata }
+}
+
+// ---------------------------
+// 顶层 CPU + ROM + RAM
+// ---------------------------
+class Top extends Module {
+  val io = IO(new Bundle {
+    val rom_addr = Output(UInt(32.W))
+    val rom_data = Input(UInt(32.W))
+    val ram_addr = Output(UInt(32.W))
+    val ram_wdata= Output(UInt(32.W))
+    val ram_rdata= Input(UInt(32.W))
+    val ram_we   = Output(Bool())
+  })
+
+  val cpu = Module(new MiniRV)
+  val rom = Module(new ROM(1024))
+  val ram = Module(new RAM(1024))
 
   // ---------------------------
-  // IF <-> ID
+  // IF: CPU 从 ROM 取指令
   // ---------------------------
-  idStage.io.instr := ifStage.io.instr
-  idStage.io.pc    := ifStage.io.pc_out
+  rom.io.addr := cpu.ifStage.pc
+  cpu.ifStage.io.instr := rom.io.data
 
   // ---------------------------
-  // ID <-> EX
+  // MEM: CPU 数据访问 RAM
   // ---------------------------
-  exStage.io.rs1    := idStage.io.rs1_data
-  exStage.io.rs2    := idStage.io.rs2_data
-  exStage.io.imm    := idStage.io.imm
-  exStage.io.alu_op := idStage.io.alu_op
-  exStage.io.jalr   := idStage.io.jalr
-  exStage.io.pc     := ifStage.io.pc_out
+  ram.io.addr := cpu.io.mem_addr
+  ram.io.wdata:= cpu.io.mem_wdata
+  ram.io.we   := cpu.io.mem_we
+  cpu.io.mem_rdata := ram.io.rdata
 
-  // ---------------------------
-  // Memory 接口
-  // ---------------------------
-  io.mem_addr  := exStage.io.mem_addr
-  io.mem_wdata := idStage.io.rs2_data
-  io.mem_we    := idStage.io.mem_write
-
-  // ---------------------------
-  // PC 更新
-  // ---------------------------
-  ifStage.io.pc_next := exStage.io.pc_next
+  // 顶层 IO
+  io.rom_addr := rom.io.addr
+  io.rom_data := rom.io.data
+  io.ram_addr := ram.io.addr
+  io.ram_wdata:= ram.io.wdata
+  io.ram_rdata:= ram.io.rdata
+  io.ram_we   := ram.io.we
 }
