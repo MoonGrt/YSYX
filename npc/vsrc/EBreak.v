4bf8c7e61f4acbb1f46e4f2db5bbb506f75a255a
tracer-ysyx <tracer@ysyx.org>
2026-02-18 22:13:26 +0800

    > sim RTL ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:13:25 up  8:13,  1 user,  load average: 3.83, 3.39, 2.60

--- npc/vsrc/EBreak.v
+++ npc/vsrc/EBreak.v
@@ -1,11 +1,11 @@
 import "DPI-C" function void ebreak(input byte code);
 
 module EBreak (
-    input enable,
+    input trap,
     input [7:0] code
 );
     always @* begin
-        if (enable)
+        if (trap)
             ebreak(code);
     end
 endmodule
