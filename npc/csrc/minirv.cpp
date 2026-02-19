5a08a58ea3afc1628c402a38fad451c59747a2d3
tracer-ysyx <tracer@ysyx.org>
2026-02-19 14:15:18 +0800

    > sim RTL ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:15:18 up 13:18,  1 user,  load average: 1.16, 0.86, 0.84

--- npc/csrc/minirv.cpp
+++ npc/csrc/minirv.cpp
@@ -7,60 +7,6 @@
 
 typedef uint32_t word_t;
 typedef uint32_t paddr_t;
-#define CONFIG_MBASE 0x80000000L
-#define CONFIG_MSIZE 0x8000000L
-constexpr int MEM_SIZE=1024*1024*128;
-
-uint8_t mem[MEM_SIZE];
-static inline bool in_pmem(paddr_t addr){
-  return addr - CONFIG_MBASE <= CONFIG_MSIZE && addr >= CONFIG_MBASE;
-}
-uint8_t* guest_to_host(paddr_t paddr){   
-  if (in_pmem(paddr)) return mem + paddr - CONFIG_MBASE;
-  else return NULL;
-}
-word_t paddr_read(paddr_t addr, int len){
-  if (addr < CONFIG_MBASE || addr >= CONFIG_MBASE + MEM_SIZE) return 0;
-  word_t result = 0;
-  switch (len){
-    case 1: result= *guest_to_host(addr); break;
-    case 2: result= *(uint16_t *)guest_to_host(addr); break;
-    case 4: result= *(uint32_t *)guest_to_host(addr); break;
-    default: return 0;
-  }
-#ifdef DEBUG
-  printf("paddr_read: addr=0x%08x, len=%d, data=0x%08x\n", addr, len, result);
-#endif
-  return result;
-}
-void paddr_write(paddr_t addr, int mask, word_t data){
-#ifdef DEBUG
-  printf("paddr_write: addr=0x%08x, mask=0x%02x, data=0x%08x\n", addr, mask, data);
-#endif
-  if (addr < CONFIG_MBASE || addr >= CONFIG_MBASE + MEM_SIZE) return;
-  for(int i = 0; i < 4; i++)
-    if(mask & (1 << i)) *guest_to_host(addr + i) = (data >> (i * 8)) & 0xff;
-}
-
-extern "C" {
-  bool is_ebreak;
-  uint8_t ebreak_code;
-  void ebreak(uint8_t code){
-    is_ebreak=true;
-    ebreak_code=code;
-  }
-  int pmem_read(int raddr){
-    raddr = raddr & ~0x3u;
-    word_t data= paddr_read(raddr, 4);
-    return data;
-  }
-  void pmem_write(int waddr, char wmask, int wdata){
-    paddr_write(waddr, wmask, wdata);
-  }
-}
-
-
-
 uint8_t *rom = NULL;
 uint8_t *ram = NULL;
 #define ROM_BASE 0x30000000L
@@ -91,6 +37,62 @@ static const uint32_t img [] = {
   0x00100073,  // ebreak
   0xdeadbeef,  // some data
 };
+
+static inline bool in_rom(paddr_t addr){
+  return addr - ROM_BASE <= ROM_SIZE && addr >= ROM_BASE;
+}
+static inline bool in_ram(paddr_t addr){
+  return addr - RAM_BASE <= RAM_SIZE && addr >= RAM_BASE;
+}
+uint8_t* guest_to_host(paddr_t paddr){
+  if (in_rom(paddr)) return rom + paddr - ROM_BASE;
+  else if (in_ram(paddr)) return ram + paddr - RAM_BASE;
+  else return NULL;
+}
+word_t paddr_read(paddr_t addr, int len){
+  if ((addr < ROM_BASE || addr >= ROM_BASE + ROM_SIZE) ||
+      (addr < RAM_BASE || addr >= RAM_BASE + RAM_SIZE)) return 0;
+  word_t result = 0;
+  switch (len){
+    case 1: result= *guest_to_host(addr); break;
+    case 2: result= *(uint16_t *)guest_to_host(addr); break;
+    case 4: result= *(uint32_t *)guest_to_host(addr); break;
+    default: return 0;
+  }
+#ifdef DEBUG
+  printf("paddr_read: addr=0x%08x, len=%d, data=0x%08x\n", addr, len, result);
+#endif
+  return result;
+}
+void paddr_write(paddr_t addr, int mask, word_t data){
+#ifdef DEBUG
+  printf("paddr_write: addr=0x%08x, mask=0x%02x, data=0x%08x\n", addr, mask, data);
+#endif
+  if (addr < RAM_BASE || addr >= RAM_BASE + RAM_SIZE) return;
+  for(int i = 0; i < 4; i++)
+    if(mask & (1 << i)) *guest_to_host(addr + i) = (data >> (i * 8)) & 0xff;
+}
+
+extern "C" {
+  bool is_ebreak;
+  uint8_t ebreak_code;
+  void ebreak(uint8_t code){
+    is_ebreak=true;
+    ebreak_code=code;
+  }
+  int pmem_read(int raddr){
+    raddr = raddr & ~0x3u;
+    word_t data= paddr_read(raddr, 4);
+    return data;
+  }
+  void pmem_write(int waddr, char wmask, int wdata){
+    paddr_write(waddr, wmask, wdata);
+  }
+}
+
+
+
+
 
 static vluint64_t sim_time = 0;
 static void tick(VMiniRVSOC* top, VerilatedVcdC* tfp){
