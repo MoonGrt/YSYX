fa3d352a3a5b3f16bce22e69a95b7adae2205a71
tracer-ysyx <tracer@ysyx.org>
2026-02-20 01:29:51 +0800

    > generate verilog ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:29:51 up 1 day, 32 min,  1 user,  load average: 0.59, 0.50, 0.55

--- npc/chisel/src/RiscV/MiniRV.scala
+++ npc/chisel/src/RiscV/MiniRV.scala
@@ -254,7 +254,7 @@ class ID extends Module {
   val imm_u = io.inst(31,12) << 12
 
   // -------- EX操作数 --------
-  io.rs1 := regfile(rs1)
+  io.rs1 := Mux(io.inst === LUI, 0.U(32.W), regfile(rs1))
   io.rs2 := regfile(rs2)
   io.imm := MuxLookup(immsel, 0.U)(Seq(
     IMMI -> imm_i,
