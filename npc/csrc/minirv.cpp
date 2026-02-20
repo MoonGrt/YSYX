897b18c49742406ef747419bfd06d4b86471302b
tracer-ysyx <tracer@ysyx.org>
2026-02-20 16:24:33 +0800

    > sim RTL ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:24:33 up 1 day,  4:39,  1 user,  load average: 0.70, 0.76, 0.67

--- npc/csrc/minirv.cpp
+++ npc/csrc/minirv.cpp
@@ -109,6 +109,7 @@ extern "C" {
 #define no_argument		    0
 #define required_argument	1
 #define optional_argument	2
+#include <getopt.h>
 extern int getopt_long(int argc, char * const argv[],  const char *optstring,  
                 const struct option *longopts, int *longindex);
 
