00f380b5d86685ece817ce6dd6e80dd74cf229c6
tracer-ysyx <tracer@ysyx.org>
2026-02-20 16:20:35 +0800

    > sim RTL ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:20:35 up 1 day,  4:35,  1 user,  load average: 0.53, 0.62, 0.61

--- npc/csrc/minirv.cpp
+++ npc/csrc/minirv.cpp
@@ -145,6 +145,27 @@ static int parse_args(int argc, char *argv[]) {
   return 0;
 }
 
+#define log_write(...) IFDEF(CONFIG_TARGET_NATIVE_ELF, \
+  do { \
+    extern FILE* log_fp; \
+    extern bool log_enable(); \
+    if (log_enable() && log_fp != NULL) { \
+      fprintf(log_fp, __VA_ARGS__); \
+      fflush(log_fp); \
+    } \
+  } while (0) \
+)
+#define _Log(...) \
+  do { \
+    printf(__VA_ARGS__); \
+    log_write(__VA_ARGS__); \
+  } while (0)
+#define ANSI_FG_BLUE    "\33[1;34m"
+#define ANSI_NONE       "\33[0m"
+#define ANSI_FMT(str, fmt) fmt str ANSI_NONE
+#define Log(format, ...) \
+    _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_BLUE) "\n", \
+        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 FILE *log_fp = NULL;
 void init_log(void) {
   log_fp = stdout;
