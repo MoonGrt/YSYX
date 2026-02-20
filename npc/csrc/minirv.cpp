d968e3ccddd837f69eaed75222097e0765848562
tracer-ysyx <tracer@ysyx.org>
2026-02-20 15:35:24 +0800

    > sim RTL ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:35:24 up 1 day,  3:50,  1 user,  load average: 0.51, 0.43, 0.47

--- npc/csrc/minirv.cpp
+++ npc/csrc/minirv.cpp
@@ -118,35 +118,46 @@ static void tick(VMiniRVSOC* top, VerilatedVcdC* tfp){
   tfp->dump(sim_time++);
 }
 
-// static int parse_args(int argc, char *argv[]) {
-//   const struct option table[] = {
-//     {"batch", no_argument      , NULL, 'b'},
-//     {"log"  , required_argument, NULL, 'l'},
-//     {"diff" , required_argument, NULL, 'd'},
-//     {"port" , required_argument, NULL, 'p'},
-//     {"help" , no_argument      , NULL, 'h'},
-//     {0      , 0                , NULL,  0 },
-//   };
-//   int o;
-//   while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
-//     switch (o) {
-//       case 'b': sdb_set_batch_mode(); break;
-//       case 'p': sscanf(optarg, "%d", &difftest_port); break;
-//       case 'l': log_file = optarg; break;
-//       case 'd': diff_so_file = optarg; break;
-//       case 1: img_file = optarg; return 0;
-//       default:
-//         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
-//         printf("\t-b, --batch       run with batch mode\n");
-//         printf("\t-l, --log=FILE    output log to FILE\n");
-//         printf("\t-d, --diff=REF_SO run DiffTest with reference REF_SO\n");
-//         printf("\t-p, --port=PORT   run DiffTest with port PORT\n");
-//         printf("\n");
-//         exit(0);
-//     }
-//   }
-//   return 0;
-// }
+#define no_argument		0
+#define required_argument	1
+#define optional_argument	2
+extern int getopt_long (int ___argc, char *__getopt_argv_const *___argv,
+      const char *__shortopts,
+            const struct option *__longopts, int *__longind)
+       __THROW __nonnull ((2, 3));
+extern int getopt_long_only (int ___argc, char *__getopt_argv_const *___argv,
+          const char *__shortopts,
+                const struct option *__longopts, int *__longind)
+      __THROW __nonnull ((2, 3));
+static int parse_args(int argc, char *argv[]) {
+  const struct option table[] = {
+    {"batch", no_argument      , NULL, 'b'},
+    {"log"  , required_argument, NULL, 'l'},
+    {"diff" , required_argument, NULL, 'd'},
+    {"port" , required_argument, NULL, 'p'},
+    {"help" , no_argument      , NULL, 'h'},
+    {0      , 0                , NULL,  0 },
+  };
+  int o;
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+    switch (o) {
+      case 'b': sdb_set_batch_mode(); break;
+      // case 'p': sscanf(optarg, "%d", &difftest_port); break;
+      // case 'l': log_file = optarg; break;
+      // case 'd': diff_so_file = optarg; break;
+      // case 1: img_file = optarg; return 0;
+      default:
+        printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+        printf("\t-b, --batch       run with batch mode\n");
+        printf("\t-l, --log=FILE    output log to FILE\n");
+        printf("\t-d, --diff=REF_SO run DiffTest with reference REF_SO\n");
+        printf("\t-p, --port=PORT   run DiffTest with port PORT\n");
+        printf("\n");
+        exit(0);
+    }
+  }
+  return 0;
+}
 
 int main(int argc, char **argv){
   if (argc < 1){
