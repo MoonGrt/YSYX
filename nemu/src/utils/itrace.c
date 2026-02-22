ab08704ec76d7aee9e5059ea8796bc41b76f5a5d
tracer-ysyx <tracer@ysyx.org>
2026-02-22 23:44:16 +0800

    > compile NEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:44:16 up 1 day, 10:16,  1 user,  load average: 0.30, 0.41, 0.48

--- nemu/src/utils/itrace.c
+++ nemu/src/utils/itrace.c
@@ -438,3 +438,13 @@ void trace_func_ret(paddr_t pc) {
   }
   call_num--;
 }
+
+void trace_dread(paddr_t addr, int len, IOMap *map) {
+  log_write("dtrace: read %10s at " FMT_PADDR ",%d\n",
+    map->name, addr, len);
+}
+
+void trace_dwrite(paddr_t addr, int len, word_t data, IOMap *map) {
+  log_write("dtrace: write %10s at " FMT_PADDR ",%d with " FMT_WORD "\n",
+    map->name, addr, len, data);
+}
