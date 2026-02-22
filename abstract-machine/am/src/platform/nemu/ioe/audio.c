e1201be7f8f416dd5ab39dd1a0726eb9e8e92368
tracer-ysyx <tracer@ysyx.org>
2026-02-23 00:26:28 +0800

    > compile NEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  00:26:28 up 7 min,  1 user,  load average: 1.09, 0.98, 0.54

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -8,40 +8,19 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
-static int sbuf_pos = 0;
-
 void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  int bufsize = inl(AUDIO_SBUF_ADDR); 
-    (*cfg) = (AM_AUDIO_CONFIG_T) {
-    .present = true,
-    .bufsize = bufsize
-  };
+  cfg->present = false;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
-  outl(AUDIO_FREQ_ADDR, ctrl->freq);
-  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
-  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
-  outl(AUDIO_INIT_ADDR, 1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = inl(AUDIO_COUNT_ADDR);
+  stat->count = 0;
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-  uint8_t *audio_data = (ctl->buf).start;
-  uint32_t sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
-  uint32_t cnt = inl(AUDIO_COUNT_ADDR);
-  uint32_t len = ((ctl->buf).end - (ctl->buf).start);
-  while(len > sbuf_size - cnt){;}
-  uint8_t *stream_buf = (uint8_t*)(uintptr_t)AUDIO_SBUF_ADDR;
-  for(int i = 0; i < len; i++) {
-    stream_buf[sbuf_pos] = audio_data[i];
-    sbuf_pos = (sbuf_pos + 1) % sbuf_size;
-  }
-  outl(AUDIO_COUNT_ADDR, inl(AUDIO_COUNT_ADDR) + len);
 }
