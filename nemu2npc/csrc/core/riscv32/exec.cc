da9e321cc9cbf1731595467cf95328a6e02579a9
tracer-ysyx <tracer@ysyx.org>
2026-02-25 20:44:01 +0800

    > compile NPC ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  20:44:01 up 10:26,  1 user,  load average: 0.56, 0.60, 0.49

--- nemu2npc/csrc/core/riscv32/exec.cc
+++ nemu2npc/csrc/core/riscv32/exec.cc
@@ -1,16 +1,16 @@
-// #include <verilated.h>
-// #include "Vcore.h"
+#include <verilated.h>
+#include "Vcore.h"
 
-// VMiniRVSOC *top = new VMiniRVSOC;
-// VerilatedVcdC *tfp = new VerilatedVcdC;
+VMiniRVSOC *top = new VMiniRVSOC;
+VerilatedVcdC *tfp = new VerilatedVcdC;
 
-// extern "C" {
-//   void rtl_init() {
-//     Verilated::commandArgs(argc, argv);
-//     Verilated::mkdir("logs");
-//   }
-//   void rtl_step() {
-//     top->clock = 0; top->eval();
-//     top->clock = 1; top->eval();
-//   }
-// }
+extern "C" {
+  void rtl_init() {
+    Verilated::commandArgs(argc, argv);
+    Verilated::mkdir("logs");
+  }
+  void rtl_step() {
+    top->clock = 0; top->eval();
+    top->clock = 1; top->eval();
+  }
+}
