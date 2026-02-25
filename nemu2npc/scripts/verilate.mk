672da7cd6b136ceeb7733989cac42e872c0e08de
tracer-ysyx <tracer@ysyx.org>
2026-02-26 01:15:54 +0800

    > compile NPC ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  01:15:54 up 14:50,  1 user,  load average: 0.20, 0.31, 0.28

--- nemu2npc/scripts/verilate.mk
+++ nemu2npc/scripts/verilate.mk
@@ -2,7 +2,7 @@ GTKWAVE ?= gtkwave
 VERILATOR ?= verilator
 VERILATOR_ROOT = /usr/local/share/verilator
 VERILATOR_CFLAGS += --trace -cc -MMD -cc -O3 --x-assign fast \
-                    --x-initial fast -Wno-UNOPTFLAT \
+                    --x-initial fast \
                     --timescale "1ns/1ns" --no-timing \
                     -CFLAGS -ggdb -LDFLAGS -ggdb -j 8
 
