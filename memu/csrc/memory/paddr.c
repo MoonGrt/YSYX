d00c533cd8c31c73771a6704a041b11aca0b9d7c
tracer-ysyx <tracer@ysyx.org>
2026-02-26 22:54:42 +0800

    > compile MEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  22:54:42 up 17 min,  1 user,  load average: 1.19, 0.77, 0.55

--- memu/csrc/memory/paddr.c
+++ memu/csrc/memory/paddr.c
@@ -38,7 +38,7 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
 }
 
 static void out_of_bound(paddr_t addr) {
-  panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
+  Log("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
 }
 
@@ -55,7 +55,7 @@ word_t paddr_read(paddr_t addr, int len) {
   IFDEF(CONFIG_MTRACE, display_pread(addr, len));
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  // out_of_bound(addr);
+  out_of_bound(addr);
   return 0;
 }
 
@@ -63,5 +63,5 @@ void paddr_write(paddr_t addr, int len, word_t data) {
   IFDEF(CONFIG_MTRACE, display_pwrite(addr, len, data));
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
-  // out_of_bound(addr);
+  out_of_bound(addr);
 }
