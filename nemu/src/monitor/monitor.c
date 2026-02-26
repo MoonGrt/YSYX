cfdae81891691f91a532e8205ea5631bcd5122e6
tracer-ysyx <tracer@ysyx.org>
2026-02-26 23:50:49 +0800

    > compile NEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  23:50:49 up  1:14,  1 user,  load average: 0.33, 0.42, 0.52

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -41,6 +41,7 @@ static void welcome() {
 void sdb_set_batch_mode();
 
 static char *log_file = NULL;
+static char *flog_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
 static char *elf_file = NULL;
@@ -72,6 +73,7 @@ static int parse_args(int argc, char *argv[]) {
   const struct option table[] = {
     {"batch", no_argument      , NULL, 'b'},
     {"log"  , required_argument, NULL, 'l'},
+    {"flog" , required_argument, NULL, 'f'},
     {"diff" , required_argument, NULL, 'd'},
     {"port" , required_argument, NULL, 'p'},
     {"help" , no_argument      , NULL, 'h'},
@@ -79,11 +81,12 @@ static int parse_args(int argc, char *argv[]) {
     {0      , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "-bhl:f:d:p:e:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
+      case 'f': flog_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
       case 'e': elf_file = optarg; break;
       case 1: img_file = optarg; return 0;
@@ -91,6 +94,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b, --batch       run with batch mode\n");
         printf("\t-l, --log=FILE    output log to FILE\n");
+        printf("\t-f, --flog=FILE   output flog to FILE\n");
         printf("\t-d, --diff=REF_SO run DiffTest with reference REF_SO\n");
         printf("\t-p, --port=PORT   run DiffTest with port PORT\n");
         printf("\t-e, --elf=ELF     ELF File for ftrace\n");
