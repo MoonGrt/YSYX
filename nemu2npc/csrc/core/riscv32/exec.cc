9ff708809aabe25a3511951c9f0b7eb9ebcafd3a
tracer-ysyx <tracer@ysyx.org>
2026-02-26 13:09:51 +0800

    > compile NPC ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  13:09:51 up 16:22,  1 user,  load average: 0.30, 0.37, 0.36

--- nemu2npc/csrc/core/riscv32/exec.cc
+++ nemu2npc/csrc/core/riscv32/exec.cc
@@ -45,13 +45,10 @@ extern "C" {
     Verilated::gotFinish(true);
   }
   int pmem_read(int raddr){
-#ifdef DEBUG
-    printf("paddr_read:  addr=0x%08x", raddr);
-#endif
     raddr = raddr & ~0x3u;
     word_t data= paddr_read(raddr, 4);
 #ifdef DEBUG
-    printf(",   data=0x%08x\n", data);
+    printf("paddr_read:  addr=0x%08x,   data=0x%08x\n", raddr, data);
 #endif
     return data;
   }
