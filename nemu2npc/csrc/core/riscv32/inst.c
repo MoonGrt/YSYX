fd01e57e785d39637490b60fdad5c1e075e179ce
tracer-ysyx <tracer@ysyx.org>
2026-02-26 20:15:39 +0800

    > compile NPC ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  20:15:39 up 23:28,  1 user,  load average: 0.50, 0.44, 0.43

--- nemu2npc/csrc/core/riscv32/inst.c
+++ nemu2npc/csrc/core/riscv32/inst.c
@@ -165,7 +165,6 @@ static int decode_exec(Decode *s) {
 }
 
 int isa_exec_once(Decode *s) {
-  printf("exec_once\n");
   // rtl_step();
   s->isa.inst = inst_fetch(&s->snpc, 4);
   IFDEF(CONFIG_ITRACE, trace_inst(s->pc, s->isa.inst));
