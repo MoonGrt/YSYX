7cc53152d1023588cfa01d4b60f80349fd4de72d
tracer-ysyx <tracer@ysyx.org>
2026-02-27 14:36:53 +0800

    > compile NEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  14:36:53 up  4:42,  1 user,  load average: 0.48, 0.40, 0.37

--- memu/csrc/core/riscv32/difftest/dut.c
+++ memu/csrc/core/riscv32/difftest/dut.c
@@ -20,8 +20,7 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   bool result = true;
   if (ref_r->pc != pc) {
-    printf("new pc is different at " FMT_WORD "! ref: " FMT_WORD
-           ", NPC: " FMT_WORD "\n", pc, ref_r->pc, pc);
+    printf("new pc is different at " FMT_WORD "! ref: " FMT_WORD "\n", pc, ref_r->pc);
     result = false;
   }
   for(int i = 0; i < 32; i++) {
