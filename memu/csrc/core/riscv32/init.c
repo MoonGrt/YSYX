bdf4c1aed03e378f6cef52a2390d70870f0944bf
tracer-ysyx <tracer@ysyx.org>
2026-02-27 15:29:23 +0800

    > compile MEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:29:23 up  5:35,  1 user,  load average: 0.45, 0.33, 0.44

--- memu/csrc/core/riscv32/init.c
+++ memu/csrc/core/riscv32/init.c
@@ -30,6 +30,13 @@ static const uint32_t img [] = {
   0xdeadbeef,  // 0x08 deadbeef
 };
 
+static void restart() {
+  /* Set the initial program counter. */
+  cpu.pc = RESET_VECTOR;
+  /* The zero register is always 0. */
+  cpu.gpr[0] = 0;
+}
+
 void init_isa() {
   /* Load built-in image. */
   memcpy(guest_to_host(RESET_VECTOR), img, sizeof(img));
