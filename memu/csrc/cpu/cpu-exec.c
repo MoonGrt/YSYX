31bd14c08803ef54bd8648d5f80a80c8ad6a56f7
tracer-ysyx <tracer@ysyx.org>
2026-02-27 19:10:09 +0800

    > compile MEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  19:10:09 up  9:11,  1 user,  load average: 1.78, 0.69, 0.53

--- memu/csrc/cpu/cpu-exec.c
+++ memu/csrc/cpu/cpu-exec.c
@@ -101,20 +101,9 @@ static void exec_once(Decode *s, vaddr_t pc) {
 static void execute(uint64_t n) {
   Decode s;
   for (;n > 0; n --) {
-#if defined(CONFIG_NEMU)
-#elif defined(CONFIG_NPC)
-    s.pc = RTL_Decode.pc;
-    s.snpc = RTL_Decode.snpc;
-    s.dnpc = RTL_Decode.dnpc;
-    s.isa.inst = RTL_Decode.isa.inst;
-    trace_and_difftest(&s, cpu.pc);
-#endif
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
-#if defined(CONFIG_NEMU)
     trace_and_difftest(&s, cpu.pc);
-#elif defined(CONFIG_NPC)
-#endif
     if (nemu_state.state != MEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
