37f37a7216a3634c04ff30e29271fbac8ae06196
tracer-ysyx <tracer@ysyx.org>
2026-03-01 17:33:28 +0800

    > generate verilog ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:33:28 up 16:36,  1 user,  load average: 1.53, 3.25, 3.11

--- memu/chisel/src/RiscV/RISCV32E.scala
+++ memu/chisel/src/RiscV/RISCV32E.scala
@@ -146,9 +146,6 @@ class Riscv32E_ID extends Module {
   val imm_i_sext = Cat(Fill(20, imm_i(11)), imm_i)
   val imm_s = Cat(io.inst(31, 25), io.inst(11, 7))  // imm for S-type
   val imm_s_sext = Cat(Fill(20, imm_s(11)), imm_s)
-  // Decode imm of B-type instruction
-  val imm_b = Cat(io.inst(31), io.inst(7), io.inst(30, 25), io.inst(11, 8))
-  val imm_b_sext = Cat(Fill(19, imm_b(11)), imm_b, 0.U(1.U))
   // Decode imm of J-type instruction
   val imm_j = Cat(io.inst(31), io.inst(19, 12), io.inst(20), io.inst(30, 21))
   val imm_j_sext = Cat(Fill(11, imm_j(19)), imm_j, 0.U(1.U))  // Set LSB to zero
