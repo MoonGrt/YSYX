2c4eca3e4b9cc47575162ffcc87ffd50eabd6a3e
tracer-ysyx <tracer@ysyx.org>
2026-03-01 18:45:39 +0800

    > generate verilog ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  18:45:39 up 17:49,  1 user,  load average: 1.97, 1.93, 1.64

--- memu/chisel/src/RiscV/RISCV32E.scala
+++ memu/chisel/src/RiscV/RISCV32E.scala
@@ -104,6 +104,7 @@ class Riscv32E_ID extends Module {
     val exsel   = Output(UInt(EX_SEL_LEN.W))
     val op1     = Output(UInt(32.W))
     val op2     = Output(UInt(32.W))
+    val rs2     = Output(UInt(32.W))
     val rd_addr = Output(UInt(5.W))
 
     // Control signals
