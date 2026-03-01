60f0b246f9832c64bf3f6818d7d50d08723253f0
tracer-ysyx <tracer@ysyx.org>
2026-03-01 19:19:16 +0800

    > generate verilog ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  19:19:16 up 18:22,  1 user,  load average: 4.22, 2.84, 2.23

--- memu/chisel/src/Elaborate.scala
+++ memu/chisel/src/Elaborate.scala
@@ -29,7 +29,9 @@ object Riscv32ETOP extends App {
     "--lowering-options=" + List(
       // make yosys happy
       // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
-      "disallowLocalVariables",
+
+      "disallowPackedArrays",
+      "locationInfoStyle=wrapInAtSquareBracket"
     ).reduce(_ + "," + _)
   )
   circt.stage.ChiselStage.emitSystemVerilogFile(new riscv.Riscv32ETOP(), args, firtoolOptions)
