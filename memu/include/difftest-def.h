f5b5d17f673de9c94749bab256351691a7acf982
tracer-ysyx <tracer@ysyx.org>
2026-03-01 15:56:37 +0800

    > compile MEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:56:37 up 15:10,  1 user,  load average: 0.20, 0.21, 0.28

--- memu/include/difftest-def.h
+++ memu/include/difftest-def.h
@@ -46,6 +46,10 @@ enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
 #define RISCV_GPR_TYPE MUXDEF(CONFIG_RV64, uint64_t, uint32_t)
 #define RISCV_GPR_NUM  MUXDEF(CONFIG_RVE , 16, 32)
 #define DIFFTEST_REG_SIZE (sizeof(RISCV_GPR_TYPE) * (RISCV_GPR_NUM + 1)) // GPRs + pc
+#elif defined(CONFIG_CORE_riscv32e)
+#define RISCV_GPR_TYPE MUXDEF(CONFIG_RV64, uint64_t, uint32_t)
+#define RISCV_GPR_NUM  MUXDEF(CONFIG_RVE , 16, 32)
+#define DIFFTEST_REG_SIZE (sizeof(RISCV_GPR_TYPE) * (RISCV_GPR_NUM + 1)) // GPRs + pc
 #elif defined(CORE_riscv64)
 
 #else
