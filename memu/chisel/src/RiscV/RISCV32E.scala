5a5dc7e6e637e78e23fb523dda663ca57e0f05a9
tracer-ysyx <tracer@ysyx.org>
2026-03-03 21:17:25 +0800

    > generate verilog ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:17:25 up 1 day,  1:46,  1 user,  load average: 0.84, 0.93, 0.79

--- memu/chisel/src/RiscV/RISCV32E.scala
+++ memu/chisel/src/RiscV/RISCV32E.scala
@@ -241,14 +241,7 @@ class Riscv32E_ID extends Module {
 
   // -------- 寄存器堆 --------
   val GPR = RegInit(VecInit(Seq.fill(32)(0.U(WORD_LEN.W))))
-  val CSR = RegInit(VecInit(Seq(  // 注意修改 csrOut 个数
-    0x00000000.U,  // mstatus
-    0x00000000.U,  // mepc
-    0x00000000.U,  // mcause
-    0x00000000.U,  // mtvec
-    // 0x00000000.U,  // mvendorid
-    // 0x00000000.U,  // marchid
-  )))
+  val CSR = RegInit(VecInit(Seq.fill(4)(0.U(WORD_LEN.W))))
 
   // -------- 指令字段 --------
   val rd  = io.inst(11,7)
