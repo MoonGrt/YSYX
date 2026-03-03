2d90d5a36f155a13ad374bc885bbdb6c1dfa8ca7
tracer-ysyx <tracer@ysyx.org>
2026-03-03 23:07:53 +0800

    > generate verilog ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  23:07:53 up 1 day,  3:36,  1 user,  load average: 3.31, 3.55, 3.16

--- memu/chisel/src/RiscV/RISCV32E.scala
+++ memu/chisel/src/RiscV/RISCV32E.scala
@@ -455,7 +455,8 @@ class Riscv32E extends Module {
   io.mem_addr  := exStage.io.aluout
   io.mem_wdata := idStage.io.rs2
   val memBen    = ~reset.asBool && ((idStage.io.memsel === MEM_WB) || (idStage.io.memsel === MEM_RB) || (idStage.io.memsel === MEM_RBU))
-  io.mem_len   := Mux(memBen, 1.U, 4.U)
+  val memHen    = ~reset.asBool && ((idStage.io.memsel === MEM_WH) || (idStage.io.memsel === MEM_RH) || (idStage.io.memsel === MEM_RHU))
+  io.mem_len   := Mux(memBen, 1.U, Mux(memBen, 2.U, 4.U))
 
   // Write Back
   idStage.io.wb_en   := idStage.io.regWen
