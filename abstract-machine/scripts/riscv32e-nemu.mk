4c0e36ac77e55ceb9d176e345c28ef2f07acda67
tracer-ysyx <tracer@ysyx.org>
2026-03-03 21:59:07 +0800

    > generate verilog ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:59:07 up 1 day,  2:28,  1 user,  load average: 0.76, 2.57, 3.60

--- abstract-machine/scripts/riscv32e-nemu.mk
+++ abstract-machine/scripts/riscv32e-nemu.mk
@@ -1,8 +1,8 @@
 include $(AM_HOME)/scripts/isa/riscv.mk
 include $(AM_HOME)/scripts/platform/nemu.mk
 CFLAGS  += -DISA_H=\"riscv/riscv.h\"
-COMMON_CFLAGS += -march=rv32m_zicsr -mabi=ilp32e  # overwrite
-LDFLAGS       += -melf32lriscv                    # overwrite
+COMMON_CFLAGS += -march=rv32em_zicsr -mabi=ilp32e  # overwrite
+LDFLAGS       += -melf32lriscv                     # overwrite
 
 AM_SRCS += riscv/nemu/start.S \
            riscv/nemu/cte.c \
