372890e172714d71fbc8c9d921c285ed58d94c98
tracer-ysyx <tracer@ysyx.org>
2026-03-07 21:53:51 +0800

    > compile MEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:53:51 up  7:18,  1 user,  load average: 0.26, 0.45, 0.46

--- memu/csrc/device/serial.c
+++ memu/csrc/device/serial.c
@@ -23,16 +23,16 @@
 
 static uint8_t *serial_base = NULL;
 
-static void serial_putc(char ch) {
-  MUXDEF(CONFIG_TARGET_AM, putch(ch), putc(ch, stderr));
-}
+// static void serial_putc(char ch) {
+//   MUXDEF(CONFIG_TARGET_AM, putch(ch), putc(ch, stderr));
+// }
 
 static void serial_io_handler(uint32_t offset, int len, bool is_write) {
   assert(len == 1);
   switch (offset) {
     /* We bind the serial port with the host stderr in MEMU. */
     case CH_OFFSET:
-      if (is_write) serial_putc(serial_base[0]);
+      if (is_write) printf("hello world\n");
       else panic("do not support read");
       break;
     default: panic("do not support offset = %d", offset);
