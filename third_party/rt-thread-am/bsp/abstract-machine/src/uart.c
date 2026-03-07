46899010e38df8de65c7503246e08cebdd5baf1b
tracer-ysyx <tracer@ysyx.org>
2026-03-07 23:52:46 +0800

    > compile NEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  23:52:46 up  9:10,  1 user,  load average: 1.07, 0.61, 0.45

---
+++ third_party/rt-thread-am/bsp/abstract-machine/src/uart.c
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2006-2020, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ */
+
+#include <rtdevice.h>
+#include <rtthread.h>
+#include <am.h>
+#include <klib.h>
+#include <klib-macros.h>
+
+#define UART_DEFAULT_BAUDRATE 115200
+
+struct device_uart {
+  rt_ubase_t hw_base;
+  rt_uint32_t irqno;
+};
+
+static void *uart0_base = (void*)0x10000000;
+static struct rt_serial_device serial0;
+static struct device_uart uart0;
+
+static rt_err_t _uart_configure(struct rt_serial_device *serial, struct serial_configure *cfg) {
+  return (RT_EOK);
+}
+
+static rt_err_t _uart_control(struct rt_serial_device *serial, int cmd, void *arg) {
+  return (RT_EOK);
+}
+
+static int _uart_putc(struct rt_serial_device *serial, char c) {
+  putch(c);
+  return 1;
+}
+
+static bool has_uart, has_kbd;
+static int _uart_getc(struct rt_serial_device *serial) {
+  /* 1 先读 UART */
+  if (has_uart) {
+    AM_UART_RX_T rx = io_read(AM_UART_RX);
+    if (rx.data != (char)-1)
+      return rx.data;
+  }
+  /* 2 再读 keyboard */
+  if (has_kbd) {
+    AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+    if (ev.keydown) {
+      switch (ev.keycode) {
+        AM_KEY_TO_CHAR(AM_KEY_CHAR_MAP)
+        default: return 0;  // 其他按键返回0
+      }
+    }
+  }
+  return -1;
+}
+
+const struct rt_uart_ops _uart_ops = {
+  _uart_configure,
+  _uart_control,
+  _uart_putc,
+  _uart_getc,
+  // TODO: add DMA support
+  RT_NULL};
+
+/*
+ * UART Initiation
+ */
+int rt_hw_uart_init(void) {
+  has_uart = io_read(AM_UART_CONFIG).present;
+  has_kbd  = io_read(AM_INPUT_CONFIG).present;
+
+  struct rt_serial_device *serial;
+  struct device_uart *uart;
+  struct serial_configure config = RT_SERIAL_CONFIG_DEFAULT;
+  // register device
+  serial = &serial0;
+  uart = &uart0;
+
+  serial->ops = &_uart_ops;
+  serial->config = config;
+  serial->config.baud_rate = UART_DEFAULT_BAUDRATE;
+  uart->hw_base = (rt_ubase_t)uart0_base;
+  uart->irqno = 0x0a;
+
+  rt_hw_serial_register(serial,
+      RT_CONSOLE_DEVICE_NAME,
+      RT_DEVICE_FLAG_STREAM | RT_DEVICE_FLAG_RDWR | RT_DEVICE_FLAG_INT_RX,
+      uart);
+  return 0;
+}
+
+/* WEAK for SDK 0.5.6 */
+rt_weak void uart_debug_init(int uart_channel)
+{
+}
