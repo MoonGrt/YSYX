86d238f481f0a80f15711309ad8a239d3ea45c20
MoonGrt <1561145394@qq.com>
2026-03-07 23:57:50 +0800

    rt-thread-am impl command input; ab add keyboard to ascii mapping

--- memu/csrc/device/serial.c
+++ memu/csrc/device/serial.c
@@ -23,7 +23,6 @@
 
 static uint8_t *serial_base = NULL;
 
-
 static void serial_putc(char ch) {
   MUXDEF(CONFIG_TARGET_AM, putch(ch), putc(ch, stderr));
 }
@@ -33,7 +32,10 @@ static void serial_io_handler(uint32_t offset, int len, bool is_write) {
   switch (offset) {
     /* We bind the serial port with the host stderr in MEMU. */
     case CH_OFFSET:
-      if (is_write) serial_putc(serial_base[0]);
+      if (is_write) {
+        serial_putc(serial_base[0]);
+        // printf("Hello World!\n");
+      }
       else panic("do not support read");
       break;
     default: panic("do not support offset = %d", offset);
@@ -47,5 +49,4 @@ void init_serial() {
 #else
   add_mmio_map("serial", CONFIG_SERIAL_MMIO, serial_base, 8, serial_io_handler);
 #endif
-
 }
