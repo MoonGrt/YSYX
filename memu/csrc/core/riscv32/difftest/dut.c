ef404b9b886a406ed28b528801830e16d375e5ec
tracer-ysyx <tracer@ysyx.org>
2026-03-08 12:56:00 +0800

    > compile MEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  12:56:00 up 11:39,  1 user,  load average: 0.28, 0.56, 0.84

--- memu/csrc/core/riscv32/difftest/dut.c
+++ memu/csrc/core/riscv32/difftest/dut.c
@@ -29,15 +29,31 @@
     result = false; \
   }
 
+#define CSR_LIST(_) \
+  _(mepc)           \
+  _(mstatus)        \
+  _(mcause)         \
+  _(mtvec)          \
+  _(mcycle)         \
+  _(mcycleh)        \
+  _(mvendorid)      \
+  _(marchid)
+
+#define CHECKDIFF_CSR(p) \
+  if (ref_r->csr.p != cpu.csr.p) { \
+    printf("difftest fail at csr." #p ", expect " FMT_WORD " got " FMT_WORD "\n", \
+           ref_r->csr.p, cpu.csr.p); \
+    result = false; \
+  }
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   bool result = true;
   CHECKDIFF(pc);
   for (int i = 0; i < 32; i++)
     CHECKDIFF_FMT(gpr[i], "gpr[%d]", i);
-  CHECKDIFF(csr.mstatus);
-  CHECKDIFF(csr.mcause);
-  CHECKDIFF(csr.mepc);
-  CHECKDIFF(csr.mtvec);
+#define _(x) CHECKDIFF_CSR(x);
+  CSR_LIST(_)
+#undef _
   return result;
 }
 
