a1ae435746899e6ee14635fde62bb23af9ce1a89
tracer-ysyx <tracer@ysyx.org>
2026-03-08 14:27:11 +0800

    > compile MEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  14:27:11 up 13:11,  1 user,  load average: 0.58, 0.72, 1.10

--- memu/tools/spike-diff/difftest.cc
+++ memu/tools/spike-diff/difftest.cc
@@ -84,8 +84,8 @@ void sim_t::diff_set_regs(void* diff_context) {
     state->XPR.write(i, (sword_t)ctx->gpr[i]);
   }
   state->pc = ctx->pc;
-  // state->mvendorid->write(0x79737978);
-  state->marchid->write(0x018CE26E);
+  state->mvendorid->write(ctx->csr->mvendorid);
+  state->marchid->write(ctx->csr->marchid);
 }
 
 void sim_t::diff_memcpy(reg_t dest, void* src, size_t n) {
