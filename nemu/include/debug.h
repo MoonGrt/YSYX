8fd513cd5c57aecaab12eb77be972ecaa6237891
tracer-ysyx <tracer@ysyx.org>
2026-03-11 21:51:33 +0800

    > compile NEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:51:33 up 16:57,  1 user,  load average: 0.62, 0.51, 0.44

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -23,7 +23,7 @@
 #define Log(format, ...) \
     _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_BLUE) "\n", \
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
-
+volatile static int dummy;
 #define Assert(cond, format, ...) \
   do { \
     if (!(cond)) { \
