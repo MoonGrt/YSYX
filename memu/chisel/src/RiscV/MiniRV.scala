c7acd72a504e870dc937293f127828a9cf4c2e0c
tracer-ysyx <tracer@ysyx.org>
2026-03-12 21:33:12 +0800

    > generate verilog ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:33:12 up 17:45,  1 user,  load average: 4.71, 4.51, 3.72

--- memu/chisel/src/RiscV/MiniRV.scala
+++ memu/chisel/src/RiscV/MiniRV.scala
@@ -8,33 +8,7 @@ import Constants._
 // ---------------------------
 // Configuration constants
 // ---------------------------
-object MiniRV_Constants {
-  val IMM_SEL_LEN = 2
-  val IMMN = 0.U(IMM_SEL_LEN.W)
-  val IMMI = 1.U(IMM_SEL_LEN.W)
-  val IMMS = 2.U(IMM_SEL_LEN.W)
-  val IMMU = 3.U(IMM_SEL_LEN.W)
 
-  val EX_SEL_LEN = 1
-  val EX_ADD  = 0.U(EX_SEL_LEN.W)
-  val EX_JALR = 1.U(EX_SEL_LEN.W)
-
-  val JUMP_SEL_LEN = 1
-  val JUMP_NONE = 0.U(JUMP_SEL_LEN.W)
-  val JUMP_JALR = 1.U(JUMP_SEL_LEN.W)
-
-  val WB_SEL_LEN = 2
-  val WB_NONE = 0.U(WB_SEL_LEN.W)
-  val WB_EX   = 1.U(WB_SEL_LEN.W)
-  val WB_MEM  = 2.U(WB_SEL_LEN.W)
-
-  val MEM_SEL_LEN = 3
-  val MEM_NONE = 0.U(MEM_SEL_LEN.W)
-  val MEM_RW   = 1.U(MEM_SEL_LEN.W)  // write word
-  val MEM_RB   = 2.U(MEM_SEL_LEN.W)  // write byte
-  val MEM_WW   = 3.U(MEM_SEL_LEN.W)
-  val MEM_WB   = 4.U(MEM_SEL_LEN.W)
-}
 
 // ---------------------------
 // IF 模块：Instruction Fetch
