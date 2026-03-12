2759f593614994a9dab258015eec35aa1ae58a94
tracer-ysyx <tracer@ysyx.org>
2026-03-13 00:19:20 +0800

    > generate verilog ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  00:19:20 up 20:31,  1 user,  load average: 4.23, 4.23, 4.68

--- memu/chisel/src/RiscV/RISCV32E.scala
+++ memu/chisel/src/RiscV/RISCV32E.scala
@@ -347,16 +347,15 @@ class Riscv32E extends Module {
   exStage.io.immsb := idStage.io.immsb
   exStage.io.exsel := idStage.io.exsel
 
-  // Memory control
-  val isLoad  = Seq(MEM.RW, MEM.RH, MEM.RB, MEM.RHU, MEM.RBU).map(idStage.io.memsel === _).reduce(_||_)
-  val isStore = Seq(MEM.WW, MEM.WH, MEM.WB).map(idStage.io.memsel === _).reduce(_||_)
-  val isByte  = Seq(MEM.WB, MEM.RB, MEM.RBU).map(idStage.io.memsel === _).reduce(_||_)
-  val isHalf  = Seq(MEM.WH, MEM.RH, MEM.RHU).map(idStage.io.memsel === _).reduce(_||_)
-  io.mem_re    := isLoad
-  io.mem_we    := isStore
+  // Memory
+  io.mem_re    := (idStage.io.memsel === MEM.RW) || (idStage.io.memsel === MEM.RH) || (idStage.io.memsel === MEM.RB) ||
+                  (idStage.io.memsel === MEM.RHU) || (idStage.io.memsel === MEM.RBU)
+  io.mem_we    := (idStage.io.memsel === MEM.WW) || (idStage.io.memsel === MEM.WH) || (idStage.io.memsel === MEM.WB)
   io.mem_addr  := exStage.io.aluout
   io.mem_wdata := idStage.io.rs2
-  io.mem_len   := Mux(isByte, 1.U, Mux(isHalf, 2.U, 4.U))
+  val memBen    = ~reset.asBool && ((idStage.io.memsel === MEM.WB) || (idStage.io.memsel === MEM.RB) || (idStage.io.memsel === MEM.RBU))
+  val memHen    = ~reset.asBool && ((idStage.io.memsel === MEM.WH) || (idStage.io.memsel === MEM.RH) || (idStage.io.memsel === MEM.RHU))
+  io.mem_len   := Mux(memBen, 1.U, Mux(memHen, 2.U, 4.U))
 
   // Write Back
   idStage.io.wb_en   := idStage.io.regWen
@@ -385,9 +384,11 @@ class Riscv32ETOP extends Module {
   val cpu = Module(new Riscv32E)
   val rom = Module(new ROM_DPI)
   val ram = Module(new RAM_DPI)
+
   // 指令取值
   rom.io.addr := cpu.io.pc
   cpu.io.inst := rom.io.data
+
   // 数据访存
   ram.io.clk   := clock
   ram.io.re    := cpu.io.mem_re
