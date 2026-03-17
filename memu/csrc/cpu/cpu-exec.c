2e96a8dd5e9d1d15fbff818b47df5bb08880f253
tracer-ysyx <tracer@ysyx.org>
2026-03-17 17:42:28 +0800

    > compile MEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:42:28 up 1 day, 16:03,  1 user,  load average: 0.25, 0.24, 0.17

--- memu/csrc/cpu/cpu-exec.c
+++ memu/csrc/cpu/cpu-exec.c
@@ -64,6 +64,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 
 void trace_inst(word_t pc, uint32_t inst);
 static void exec_once(Decode *s, vaddr_t pc) {
+  printf("exec_once: pc = " FMT_WORD "\n", pc);
   s->pc = pc;
   s->snpc = pc;
   isa_exec_once(s);
