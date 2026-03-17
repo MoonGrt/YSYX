ad08556fe2dd92153368b5560991e92faff10d59
tracer-ysyx <tracer@ysyx.org>
2026-03-17 14:18:02 +0800

    > compile MEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  14:18:02 up 1 day, 12:39,  1 user,  load average: 0.49, 0.32, 0.16

--- am-kernels/kernels/typing-game/game.c
+++ am-kernels/kernels/typing-game/game.c
@@ -141,16 +141,13 @@ int main() {
   uint64_t t0 = io_read(AM_TIMER_UPTIME).us;
   while (1) {
     int frames = (io_read(AM_TIMER_UPTIME).us - t0) / (1000000 / FPS);
-    for (; current < frames; current++) {
+    for (; current < frames; current++)
       game_logic_update(current);
-    }
     while (1) {
       AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
       if (ev.keycode == AM_KEY_NONE) break;
       if (ev.keydown && ev.keycode == AM_KEY_ESCAPE) halt(0);
-      if (ev.keydown && lut[ev.keycode]) {
-        check_hit(lut[ev.keycode]);
-      }
+      if (ev.keydown && lut[ev.keycode]) check_hit(lut[ev.keycode]);
     };
     if (current > rendered) {
       render();
