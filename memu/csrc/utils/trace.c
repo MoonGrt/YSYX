68d044c146146a4d6e42a6eaa51bf8ecf169a9c5
tracer-ysyx <tracer@ysyx.org>
2026-03-18 12:00:24 +0800

    > compile MEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-100-generic #100~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 19 17:10:19 UTC  x86_64 x86_64 x86_64 GNU/Linux  12:00:24 up 1 day, 21:26,  1 user,  load average: 0.85, 0.97, 0.82

--- memu/csrc/utils/trace.c
+++ memu/csrc/utils/trace.c
@@ -37,7 +37,7 @@ void display_inst() {
   char *p;
   do {
     p = buf;
-    p += sprintf(buf, "\n%s" FMT_WORD ": %08x ", (i+1)%MAX_IRINGBUF==end?" --> ":"     ", iringbuf[i].pc, iringbuf[i].inst);
+    p += sprintf(buf, "%s" FMT_WORD ": %08x ", (i+1)%MAX_IRINGBUF==end?" --> ":"     ", iringbuf[i].pc, iringbuf[i].inst);
     disassemble(p, buf+sizeof(buf)-p, iringbuf[i].pc, (uint8_t *)&iringbuf[i].inst, 4);
     if ((i+1)%MAX_IRINGBUF==end) printf(ANSI_FG_MAGENTA);
     puts(buf);
