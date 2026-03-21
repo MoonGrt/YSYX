756a85d9eab0cc7d63fe20b15094068450c1b3e3
tracer-ysyx <tracer@ysyx.org>
2026-03-21 11:00:24 +0800

    > compile MEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  11:00:24 up  1:13,  1 user,  load average: 0.69, 0.59, 0.70

--- abstract-machine/am/src/platform/nemu/ioe/uart.c
+++ abstract-machine/am/src/platform/nemu/ioe/uart.c
@@ -8,14 +8,4 @@
 void __am_uart_init() {
 }
 
-void __am_uart_config(AM_UART_CONFIG_T *cfg) {
-  cfg->present = true;
-}
 
-void __am_uart_tx(AM_UART_TX_T *uart) {
-  putch(uart->data);
-}
-
-void __am_uart_rx(AM_UART_RX_T *uart) {
-  uart->data = getch();
-}
