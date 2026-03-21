1c9a0ceed621807835a62428e1739190e1f4df88
MoonGrt <1561145394@qq.com>
2026-03-21 12:21:53 +0800

    rt-thread support "shift" key; memu impl serial input; rt thread fix msh_auto_complete;

--- third_party/rt-thread-am/bsp/abstract-machine/src/uart.c
+++ third_party/rt-thread-am/bsp/abstract-machine/src/uart.c
@@ -37,26 +37,69 @@ static int _uart_putc(struct rt_serial_device *serial, char c) {
   return 1;
 }
 
+#define AM_KEY_CHAR_MAP(key, normal, shifted) \
+  case AM_KEY_##key: return shift ? (shifted) : (normal);
+
+#define AM_KEY_TO_CHAR(_) \
+  _(RETURN, '\n', '\n') \
+  _(SPACE, ' ', ' ') \
+  _(BACKSPACE, '\b', '\b') \
+  _(TAB, '\t', '\t') \
+  \
+  _(A, 'a', 'A') _(B, 'b', 'B') _(C, 'c', 'C') _(D, 'd', 'D') \
+  _(E, 'e', 'E') _(F, 'f', 'F') _(G, 'g', 'G') _(H, 'h', 'H') \
+  _(I, 'i', 'I') _(J, 'j', 'J') _(K, 'k', 'K') _(L, 'l', 'L') \
+  _(M, 'm', 'M') _(N, 'n', 'N') _(O, 'o', 'O') _(P, 'p', 'P') \
+  _(Q, 'q', 'Q') _(R, 'r', 'R') _(S, 's', 'S') _(T, 't', 'T') \
+  _(U, 'u', 'U') _(V, 'v', 'V') _(W, 'w', 'W') _(X, 'x', 'X') \
+  _(Y, 'y', 'Y') _(Z, 'z', 'Z') \
+  \
+  _(0, '0', ')') \
+  _(1, '1', '!') \
+  _(2, '2', '@') \
+  _(3, '3', '#') \
+  _(4, '4', '$') \
+  _(5, '5', '%') \
+  _(6, '6', '^') \
+  _(7, '7', '&') \
+  _(8, '8', '*') \
+  _(9, '9', '(') \
+  \
+  _(MINUS, '-', '_') \
+  _(EQUALS, '=', '+') \
+  _(LEFTBRACKET, '[', '{') \
+  _(RIGHTBRACKET, ']', '}') \
+  _(BACKSLASH, '\\', '|') \
+  _(SEMICOLON, ';', ':') \
+  _(APOSTROPHE, '\'', '"') \
+  _(COMMA, ',', '<') \
+  _(PERIOD, '.', '>') \
+  _(SLASH, '/', '?')
+
 static bool has_uart, has_kbd;
 static int _uart_getc(struct rt_serial_device *serial) {
-  /* 0 先执行自动脚本 */
+  /* 0 自动脚本 */
   static const char *p =
     "help\ndate\nversion\nfree\nps\npwd\nls\nmemtrace\nmemcheck\nutest_list\n";
   if (*p != '\0') return *(p++);
-
-  /* 1 先读 UART */
+  /* 1 UART */
   if (has_uart) {
     AM_UART_RX_T rx = io_read(AM_UART_RX);
     if (rx.data != (char)-1)
       return rx.data;
   }
-  /* 2 再读 keyboard */
+  /* 2 Keyboard */
   if (has_kbd) {
+    static int shift = 0;
     AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+    if (ev.keycode == AM_KEY_LSHIFT || ev.keycode == AM_KEY_RSHIFT) {
+      shift = ev.keydown;
+      return 0;
+    }
     if (ev.keydown) {
       switch (ev.keycode) {
         AM_KEY_TO_CHAR(AM_KEY_CHAR_MAP)
-        default: return 0;  // 其他按键返回0
+        default: return 0;
       }
     }
   }
