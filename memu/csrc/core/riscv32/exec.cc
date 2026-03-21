3ddea7ebc0189ff6dbed6fd2fc2767320bba4921
tracer-ysyx <tracer@ysyx.org>
2026-03-21 20:46:28 +0800

    > compile MEMU ysyx_26010030 MoonGrt Linux moon-vm 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  20:46:28 up 25 min,  1 user,  load average: 1.36, 1.12, 0.86

--- memu/csrc/core/riscv32/exec.cc
+++ memu/csrc/core/riscv32/exec.cc
@@ -107,9 +107,9 @@ extern "C" {
     // 创建 build 目录（如果不存在）
     Verilated::mkdir("build");
     // 创建 VCD 波形对象
-    Verilated::traceEverOn(true);  // 必须先打开 trace
     tfp->open("build/wave.vcd");
     top->trace(tfp, 99);  // 99 是 trace depth
+    Verilated::traceEverOn(true);  // 必须先打开 trace
     // 复位
     reset();
   }
